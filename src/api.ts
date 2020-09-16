15d4e1baf4d1da995cb9e4eaf4cb3b81c162311a
Ян Бродецкий <ianbrode@MacBook-Pro-An.local>
2020-09-16 13:43:54 +0300

    Add Docker+nginx

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
-
-const appId = process.env.REACT_APP_ID;
-const domain = process.env.REACT_APP_API_DOMAIN;
+const appId = (window as any)._env_.REACT_APP_ID;
+const domain = (window as any)._env_.REACT_APP_API_DOMAIN;
 
 export const api = {
   get: (url: string) => axios.get(url),
---
+++ src/index.d.ts
@@ -0,0 +1,7 @@
+declare global {
+  interface Window {
+    _env_: any;
+  }
+}
+
+window._env_ = window._env_ || {};
